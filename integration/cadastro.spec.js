d5252bee3d626a5fd90d15d35d67e002e097af13
Kevin.mss <kevin.mss@hotmail.com>
2022-06-25 12:16:48 -0300

    Working With Fixtures
    Realizando uma forma externa de trabalhar com massas de dados.

--- integration/cadastro.spec.js
+++ integration/cadastro.spec.js
@@ -2,61 +2,24 @@ import signup from '../pages/SignupPage'
 
 describe('Cadastro', () => {
 
+
+    beforeEach(function(){
+        cy.fixture('deliver').then((d)=>{
+            this.deliver = d
+        })
+    })
     
-
-    it('Usuario deve se tornar um entregador', () => {
-        var deliver = {
-            name: 'Kevin Silva',
-            cpf: '10210210456',
-            email: 'kevin.mss@hotmail.com',
-            whatsapp: '11941293941',
-            address: {
-                postalcode: '04534011',
-                street: 'Rua Joaquim Floriano',
-                number: '1000',
-                details: 'Apt 142',
-                district: 'Itaim Bibi',
-                city_state: 'São Paulo/SP'
-            },
-            delivery_metodo: 'Moto',
-            cnh: 'cnh-digital.jpg'
-
-        }
-
-        
-
+    it('Usuario deve se tornar um entregador',function()  {
         signup.go()
-        signup.fillForm(deliver)
+        signup.fillForm(this.deliver.signup)
         signup.submit()
-
         const expectMessage = 'Recebemos os seus dados. Fique de olho na sua caixa de email, pois e em breve retornamos o contato.'
         signup.modalContentShouldBe(expectMessage)
-
     })
 
-    it('CPF incorreto', () => {
-
-
-        var deliver = {
-            name: 'Kevin Silva',
-            cpf: '102102104AA',
-            email: 'kevin.mss@hotmail.com',
-            whatsapp: '11941293941',
-            address: {
-                postalcode: '04534011',
-                street: 'Rua Joaquim Floriano',
-                number: '1000',
-                details: 'Apt 142',
-                district: 'Itaim Bibi',
-                city_state: 'São Paulo/SP'
-            },
-            delivery_metodo: 'Moto',
-            cnh: 'cnh-digital.jpg'
-
-        }
-
+    it('CPF incorreto', function()  {
         signup.go()
-        signup.fillForm(deliver)
+        signup.fillForm(this.deliver.cpf_inv)
         signup.submit()
         signup.alertMessageShouldBe('Oops! CPF inválido')
     })
